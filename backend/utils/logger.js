1d777f77197db96ff1ce46596067f082cf5ca50e
khalidach <khalidplaysg@gmail.com>
2025-07-17 10:46:30 +0100

    logger.js

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,5 +1,7 @@
 // backend/utils/logger.js
 const winston = require("winston");
+const path = require("path");
+const fs = require("fs");
 
 // Define different logging levels
 const levels = {
@@ -33,16 +35,30 @@ const format = winston.format.combine(
 
 // Define the transports (console, file, etc.)
 const transports = [
-  // Log to the console
+  // Always log to the console
   new winston.transports.Console(),
-  // Log errors to a file
-  new winston.transports.File({
-    filename: "logs/error.log",
-    level: "error",
-  }),
-  // Log all messages to another file
-  new winston.transports.File({ filename: "logs/all.log" }),
 ];
+
+// Only add file transports if not in a production environment (like Vercel)
+// Vercel automatically sets NODE_ENV to 'production'
+if (process.env.NODE_ENV !== "production") {
+  const logDir = "logs";
+
+  // Create the log directory if it does not exist
+  if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+  }
+
+  transports.push(
+    // Log errors to a file
+    new winston.transports.File({
+      filename: path.join(logDir, "error.log"),
+      level: "error",
+    }),
+    // Log all messages to another file
+    new winston.transports.File({ filename: path.join(logDir, "all.log") })
+  );
+}
 
 // Create the logger instance
 const logger = winston.createLogger({
