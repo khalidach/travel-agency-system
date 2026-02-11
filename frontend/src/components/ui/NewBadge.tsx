8256233f7876a597838b7cff85d1a163f56288d4
khalidach <khalidplaysg@gmail.com>
2026-02-11 01:41:31 +0100

    implement dark mode to ui element + modal

--- frontend/src/components/ui/NewBadge.tsx
+++ frontend/src/components/ui/NewBadge.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 const NewBadge = () => {
   const { t } = useTranslation();
   return (
-    <span className="absolute top-0 ltr:right-0 rtl:left-0 px-2 text-[12px] font-semibold text-white bg-green-500 rounded-md animate-pulse text-center">
+    <span className="absolute top-0 ltr:right-0 rtl:left-0 px-2 text-[12px] font-semibold text-success-foreground bg-success rounded-md animate-pulse text-center">
       {t("new")}
     </span>
   );
